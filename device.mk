0da45021063978622d6157ea7aef6093bdf810c1
Vitaliy Tomin <highwaystar.ru@gmail.com>
2019-07-13 23:47:26 +0800

    tb8704: drop camera shim since 8704 has newer camera blobs

--- device.mk
+++ device.mk
@@ -219,11 +219,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf \
     $(LOCAL_PATH)/sensors/hals.conf:system/etc/sensors/hals.conf
 
-# Shim
-PRODUCT_PACKAGES += \
-    libshims_camera
-
-
 # WiFi
 PRODUCT_PACKAGES += \
     libqsap_sdk \
